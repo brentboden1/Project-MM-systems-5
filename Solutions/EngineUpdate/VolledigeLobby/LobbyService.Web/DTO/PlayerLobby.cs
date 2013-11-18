6febc5cb4b285ebf966dfebb43fca00862d67927
Aillan-De-Vos <aillan.devos@student.artesis.be>
2013-11-18 12:33:55 +0100

    Engine Updates 18-11-2013

---
+++ Solutions/EngineUpdate/VolledigeLobby/LobbyService.Web/DTO/PlayerLobby.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace LobbyService.Web.DTO
+{
+    [DataContract]
+    public class PlayerLobby
+    {
+        public PlayerLobby()
+        { 
+        }
+
+        public PlayerLobby(DTO.Player host)
+        {
+            HostPlayer = host;
+            Player = new List<Player>();
+            Player.Add(HostPlayer);
+        }
+
+        [DataMember]
+        public DTO.Lobby LobbyId { get; set; }
+
+        [DataMember]
+        public List<DTO.Player> Player { get; set; }
+
+        [DataMember]
+        public DTO.Player HostPlayer { get; set; }
+
+        [DataMember]
+        public bool IsAwaitingForPlayers { get; set; }
+
+        [DataMember]
+        public bool StartGame { get; set; }
+    }
+}
